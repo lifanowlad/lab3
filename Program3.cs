d7a3d3d4032176d315b9bd3434f55b76e6c4d35e
lifanowlad <151987017+lifanowlad@users.noreply.github.com>
2023-11-25 10:33:52 +0300

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите длину ряда чисел");
+int lenght = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int i = 0; i < lenght; i++)
+{
+    if (i % 5 == 0 || i % 7 == 0)
+    {
+        sum = sum + i;
+    }
+}
+Console.WriteLine($"Сумма чисел будет равна {sum}");
